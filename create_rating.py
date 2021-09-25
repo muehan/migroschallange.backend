67f2a4d0e265781e19a2ef2ab40dd6ae7f7b9e67
andre <tdc@gmx.ch>
2021-09-26 00:48:29 +0200

    use all files

--- create_rating.py
+++ create_rating.py
@@ -20,60 +20,85 @@ def CalculateScore(rating, amount):
 
 if __name__ == '__main__':
     customers = []
+    paths = [
+        'Abverkaufdaten_trx_202001.csv',
+        'Abverkaufdaten_trx_202002.csv',
+        'Abverkaufdaten_trx_202003.csv',
+        'Abverkaufdaten_trx_202004.csv',
+        # 'Abverkaufdaten_trx_202005.csv',
+        # 'Abverkaufdaten_trx_202006.csv',
+        # 'Abverkaufdaten_trx_202007.csv',
+        # 'Abverkaufdaten_trx_202008.csv',
+        # 'Abverkaufdaten_trx_202009.csv',
+        # 'Abverkaufdaten_trx_202010.csv',
+        # 'Abverkaufdaten_trx_202011.csv',
+        # 'Abverkaufdaten_trx_202012.csv',
+        # 'Abverkaufdaten_trx_202101.csv',
+        # 'Abverkaufdaten_trx_202102.csv',
+        # 'Abverkaufdaten_trx_202103.csv',
+        # 'Abverkaufdaten_trx_202104.csv',
+        # 'Abverkaufdaten_trx_202105.csv',
+        # 'Abverkaufdaten_trx_202106.csv',
+        # 'Abverkaufdaten_trx_202107.csv'
+    ]
+    file = 0
 
-    with open('../data/ShoppingCart/Abverkaufdaten_trx_202001.csv') as f:
-        next(f)
-        i = 0
-        for line in f.readlines():
-            if i % 10000 == 0:
-                print(i)
-            i = i + 1
-            columns = line.split(',')
-            # if columns[1] != "108248":
-            #     continue
-            currentCustomer = next(
-                (x for x in customers if x.id == columns[1]), None)
-            if currentCustomer == None:
-                currentCustomer = customer(columns[1], [])
-                customers.append(currentCustomer)
+    for path in paths:
+        file += 1
+        with open('../data/ShoppingCart/' + path) as f:
+            next(f)
+            i = 0
+            for line in f.readlines():
+                if i % 10000 == 0:
+                    print(i)
+                i = i + 1
+                columns = line.split(',')
+                # if columns[1] != "108248":
+                #     continue
+                currentCustomer = next(
+                    (x for x in customers if x.id == columns[1]), None)
+                if currentCustomer == None:
+                    currentCustomer = customer(columns[1], [])
+                    customers.append(currentCustomer)
 
-            currentBasket = next(
-                (x for x in currentCustomer.baskets if x.id == columns[2]), None)
-            if(currentBasket == None):
-                currentBasket = basket(
-                    columns[2],
-                    columns[6],
-                    rating(0, 0, 0),
-                    rating(0, 0, 0))
-                currentCustomer.baskets.append(currentBasket)
+                currentBasket = next(
+                    (x for x in currentCustomer.baskets if x.id == columns[2]), None)
+                if(currentBasket == None):
+                    currentBasket = basket(
+                        int(str(file) + str(columns[2])),
+                        columns[6],
+                        rating(0, 0, 0),
+                        rating(0, 0, 0))
+                    currentCustomer.baskets.append(currentBasket)
 
-            if(os.path.isfile('../data/products/products/de/' + str(columns[8]) + '.json')):
-                with open('../data/products/products/de/' + str(columns[8]) + '.json', encoding="utf8") as j:
-                    product = json.loads(j.read())
-                    if("m_check2" in product):
-                        m_check2 = product["m_check2"]
-                        if("carbon_footprint" in m_check2):
-                            carbon_footprint = m_check2["carbon_footprint"]
-                            if("ground_and_sea_cargo" in carbon_footprint):
-                                ground_and_sea_cargo = carbon_footprint["ground_and_sea_cargo"]
-                                currentRating = ground_and_sea_cargo["rating"]
-                                calculatedScore = CalculateScore(
+                if(os.path.isfile('../data/products/products/de/' + str(columns[8]) + '.json')):
+                    with open('../data/products/products/de/' + str(columns[8]) + '.json', encoding="utf8") as j:
+                        product = json.loads(j.read())
+                        if("m_check2" in product):
+                            m_check2 = product["m_check2"]
+                            if("carbon_footprint" in m_check2):
+                                carbon_footprint = m_check2["carbon_footprint"]
+                                if("ground_and_sea_cargo" in carbon_footprint):
+                                    ground_and_sea_cargo = carbon_footprint["ground_and_sea_cargo"]
+                                    currentRating = ground_and_sea_cargo["rating"]
+                                    calculatedScore = CalculateScore(
+                                        int(currentRating), float(columns[9]))
+                                    sustanable_score = calculatedScore
+                                    currentBasket.rating_co2.total += int(
+                                        sustanable_score)
+                                    currentBasket.rating_co2.count += 1
+                            if("animal_welfare" in m_check2):
+                                animal_welfare = m_check2["animal_welfare"]
+                                currentRating = animal_welfare["rating"]
+                                calcualtedAnimalScore = CalculateScore(
                                     int(currentRating), float(columns[9]))
-                                sustanable_score = calculatedScore
-                                currentBasket.rating_co2.total += int(
-                                    sustanable_score)
-                                currentBasket.rating_co2.count += 1
-                        if("animal_welfare" in m_check2):
-                            animal_welfare = m_check2["animal_welfare"]
-                            currentRating = animal_welfare["rating"]
-                            calcualtedAnimalScore = CalculateScore(
-                                int(currentRating), float(columns[9]))
-                            currentBasket.rating_animal_welfare.total += int(calcualtedAnimalScore)
-                            currentBasket.rating_animal_welfare.count += 1
-            # else:
-            #     print('product not found: ' + str(columns[8]))
-            # if(i == 100000):
-            #     break
+                                currentBasket.rating_animal_welfare.total += int(
+                                    calcualtedAnimalScore)
+                                currentBasket.rating_animal_welfare.count += 1
+                # else:
+                #     print('product not found: ' + str(columns[8]))
+                # if(i == 100000):
+                #     break
 
 for item in customers:
     item.sustainability = sustainability(
