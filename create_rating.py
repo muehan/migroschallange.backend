c3294c5e28ffd9d523689fce113c69031619827c
andre <tdc@gmx.ch>
2021-09-25 15:28:06 +0200

    create model

--- create_rating.py
+++ create_rating.py
@@ -1,21 +1,46 @@
 
-
-from models import basket, customer
+import dataclasses
+import json
+from models import basket, customer, sustainability
 
 
 if __name__ == '__main__':
     customers = []
-    baskets = []
 
     with open('../data/ShoppingCart/Abverkaufdaten_trx_202001.csv') as f:
+        i = 0
         for line in f.readlines():
+            print('start with line: ' + str(i))
+            i = i + 1
             columns = line.split(',')
-            if columns[1] == "101324":
-                customer = next((x for x in customers if x.id == columns[1]), None)
-                if customer == None:
-                    customer = customer(columns[1])      
-                    customers.append(customer)
-                basket = basket(columns[3], columns[1], 0)
+            # if columns[1] == "101324":
+            currentCustomer = next((x for x in customers if x.id == columns[1]), None)
+            if currentCustomer == None:
+                currentCustomer = customer(columns[1], [])
+                customers.append(currentCustomer)
 
-                
+            currentBasket = next((x for x in currentCustomer.baskets if x.id == columns[3]), None)
+            if(currentBasket == None):
+                currentBasket = basket(
+                    columns[3],
+                    columns[6],
+                    0,
+                    0,
+                    0.0,
+                    0.0)
+                currentCustomer.baskets.append(currentBasket)
+            
+            currentBasket.total_co2 += 10
+            
+            if(i == 1000):
+                break
 
+for item in customers:
+    item.sustainability = sustainability(
+        total_co2 = sum(b.total_co2 for b in item.baskets),
+        total_animal_wellfare = sum(b.total_animal_wellfare for b in item.baskets)
+    )
+    content = json.dumps(dataclasses.asdict(item))
+    # print(content)
+    with open('./customers/' + str(item.id) + '.json', "w") as f:
+        f.write(content)
--- models.py
+++ models.py
@@ -1,12 +1,24 @@
 
-from dataclasses import dataclass
+from dataclasses import dataclass, field
+from typing import List
 
-@dataclass
-class customer:
-    id: int
 
 @dataclass
 class basket:
     id: int
-    customer_id: int
-    score: int
+    date: str
+    total_co2: int
+    total_animal_wellfare: int
+    avaragescore_co2: float
+    avaragescore_animal_wellfare: float
+
+@dataclass
+class sustainability:
+    total_co2: int
+    total_animal_wellfare: int
+
+@dataclass
+class customer:
+    id: int
+    baskets: List[basket]
+    sustainability: sustainability = field(init=False)
